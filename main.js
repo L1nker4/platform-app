3c128a8536a578be64d94bfa63b138df29f23655
l1nker4 <l1nker4@outlook.com>
2020-07-13 21:29:23 +0800

    页面完成

--- main.js
+++ main.js
@@ -9,9 +9,21 @@ import store from '@/store'
 
 Vue.config.productionTip = false
 
-// 引入uView提供的对vuex的简写法文件
-let vuexStore = require('@/store/$u.mixin.js')
-Vue.mixin(vuexStore)
+Vue.prototype.$store = store
+
+import Request from '@/utils/luch-request/index.js'
+const http = new Request();
+
+
+http.setConfig((config) => { /* config 为默认全局配置*/
+    config.baseURL = 'http://localhost:9091/api/'; /* 根域名 */
+    config.header = {
+        Authentication: "123",
+    }
+    return config
+})
+
+Vue.prototype.$http = http;
 
 App.mpType = 'app'
 
